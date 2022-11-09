9d8bbcd63659447a4268d48b46fc6e027e55b991
MarianGmz <marianogomezdev@gmail.com>
2022-11-09 19:20:30 -0300

    Agregando rama Mariano

--- functions/formulario.py
+++ functions/formulario.py
@@ -1,17 +1,21 @@
 import streamlit as st
 from data.dataFunctions import conectar , juntarDf
 def formInicial():
-    nota = 0
-    levelUser= ''
+    
     placeholder = st.empty()
+    levelUser= ''
+
     with placeholder.form("my_form"):
+        
 
         st.write("Responde este pequeño formulario")
-        preg1 = st.selectbox("¿Que es el torque?", ('La fuerza del motor','La tracción','La velocidad de arranque'))
+        preg1 = st.radio("¿Que es el torque?", ('La fuerza del motor','La tracción','La velocidad de arranque'))
         preg2 = st.selectbox("¿Que significa HP?", ('Caballo de fuerza','Hewlet Packard','Ninguna de las anteriores'))
         preg3 = st.selectbox("¿En que unidad se mide la presión de los neumaticos de un auto?", ('Kelvin', 'Bares','mmHg'))
         preg4 = st.selectbox("un auto con motor mas grande consume más combustible que uno con motor chico...", ('Verdadero','Falso'))
         preg5 = st.selectbox("¿Cuando gasta mas compustible el vehiculo?", ('A altas RPM','A bajas RPM'))
+        
+        nota = 0
 
         if preg1 == 'La fuerza del motor':
             nota +=1
@@ -23,18 +27,25 @@ def formInicial():
             nota +=1
         if preg5 == 'A altas RPM':
             nota +=1
+        
+        st.write(nota)
         # Every form must have a submit button.
         submitted = st.form_submit_button("Responder")
+        
         if submitted:
-            st.write("preg 1", preg1, "preg 2", preg2)
+        
+            if nota <=3 and nota > 0:
+                levelUser='Novato'
+            elif nota >= 4 and nota > 0:
+                levelUser='Experto'
+            
+            st.write(levelUser)
             placeholder.empty()
-
-    if nota <=3:
-        levelUser='Novato'
-    elif nota >= 4 :
-        levelUser='Experto'
-
+            
     return levelUser
+            
+            
+            
 
 
 
